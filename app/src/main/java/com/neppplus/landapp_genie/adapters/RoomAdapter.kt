983e0472b930f78e46a9ffa560930e953e9a89f4
Geine <cho881020@gmail.com>
2021-04-26 19:58:37 +0900

    방 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/com/neppplus/landapp_genie/adapters/RoomAdapter.kt
@@ -0,0 +1,32 @@
+package com.neppplus.landapp_genie.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.neppplus.landapp_genie.R
+import com.neppplus.landapp_genie.datas.Room
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+
+    }
+
+}
