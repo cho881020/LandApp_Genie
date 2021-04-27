ba13631df63a6d909d6bc256aefd3ff44c644846
Geine <cho881020@gmail.com>
2021-04-27 20:35:45 +0900

    방 상세정보로 통째로 데이터를 넘겨서 반영

--- app/src/main/java/com/neppplus/landapp_genie/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/landapp_genie/ViewRoomDetailActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.landapp_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.landapp_genie.datas.Room
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("room") as Room
+
+        priceTxt.text = roomData.getFormmatedPrice()
+
+        descriptionTxt.text = roomData.description
+
+        addressTxt.text = roomData.address
+
+        floorTxt.text = roomData.getFormattedFloor()
+
+
     }
 }
